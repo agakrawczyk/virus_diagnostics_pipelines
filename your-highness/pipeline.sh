ae8c47fe9943778688304b304dcb9bd6cb5f8a5c
your-highness <johannes.helmuth@laborberlin.com>
2020-04-08 16:18:58 +0200

    Tool implementation and pipeline script to loop over all samples

---
+++ your-highness/pipeline.sh
@@ -0,0 +1,39 @@
+#/bin/bash
+#aga's YARA+SLIMM Pipeline
+#
+#USER NEEDS TO SPECIFY:
+
+RUNDIR=/home/helmuth/workspace/VIR/analyses/Aga/virus_diagnostics_pipelines/your-highness/testfq #/home/krawczyk/workdir/Virusseq/fastq
+OUTDIR=/opt/exchange/VIR/yara_slimm_fastq_results
+SLIMM_DB=/home/krawczyk/workdir/databases/slimm_db_C-RVDB16.0_HUMAN_GENOME.sldb
+YARA_DB=/home/krawczyk/workdir/databases/C-RVDBv16.yara.index
+
+
+###
+## FIND ALL FASTQ FILES
+FQ=$(find -L "${RUNDIR}" -name "*_R1*.fastq" -type f)
+if [ -z "$FQ" ]; then
+  echo "[$(date)]: No FASTQs found. Exiting..." >&2
+  exit 0
+else
+   echo "[$(date)]: The number of reads: ls -l | wc -l" >&2 
+   echo "[$(date)]: Found the following FASTQs:" >&2
+   echo $FQ | sort | sed -e "s/gz /gz\n/g" | sed "s/\(^.\)/\t\1/g" >&2
+fi
+
+for FR1 in ${FQ}; do
+  FR1N=${FR1##*/}
+  SAMPLEN=${FR1N/_R1*/}
+  OUTDIRS=${OUTDIR}/${SAMPLEN}
+  mkdir -p ${OUTDIRS}
+  /usr/bin/time -v -o ${OUTDIRS}/yara+slimm.time.log \
+    bash yara+slimm.sh ${FR1} ${YARA_DB} ${SLIMM_DB} ${OUTDIRS}
+done
+
+###6. Running multiqc
+echo "[$(date)]: Starting multiqc" >&2
+mkdir -p $OUTDIR/multiqc_output
+multiqc --interactive --outdir $OUTDIR --title "MultiQC YARA+SLIMM" \
+  $OUTDIR \
+  > $OUTDIR/multiqc_output.log
+echo "[$(date)]: Multiqc done" >&2
---
+++ your-highness/yara+slimm.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#
+# YARA+SLIMM Processing
+# 
+# Arguments: 
+#   $1 FASTQ Read1 Path
+#   $2 yara_mapper index
+#   $3 slimm index
+#   $4 output directory
+# 
+
+#TODO: use /usr/bin/time with -o as log file and store error output of each command separately
+#TODO: check if all used tools use 10 threads!
+
+FR1=$1
+YARA_DB=$2
+SLIMM_DB=$3
+OUTDIR=$4
+
+###
+## PRINT CONFIGURATION
+echo "[$(date)]: Running yara+slimm with the following parameters:" >&2
+echo "[$(date)]: FASTQ_R1 = ${FR1}" >&2
+echo "[$(date)]: YARA_DATABASE = ${YARA_DB}" >&2
+echo "[$(date)]: SLIMM_DATABASE = ${SLIMM_DB}" >&2
+echo "[$(date)]: OUTPUTDIR = ${OUTDIR}" >&2
+
+FR1N=${FR1##*/}
+SAMPLEN=${FR1N/_R1*/}
+
+mkdir -p $OUTDIR
+mkdir -p $OUTDIR/FLASH_OUTPUT
+mkdir -p $OUTDIR/slimm_reports
+mkdir -p $OUTDIR/FASTQC
+
+### PREPARE THE SHELL 
+echo "[$(date)]: Preparing shell." >&2
+conda activate vir-agafix
+ulimit -S -m 250000000
+#ulimit -a
+
+###0 QA
+/usr/bin/time -v fastqc --verbose --threads 10 \
+  --outdir $OUTDIR/FASTQC --adapters /opt/resources/fastqc_adapter_list.txt \
+  --contaminants /opt/resources/fastqc_contaminant_list.txt \
+  ${FR1} ${FR1/_R1/_R2} \
+  > $OUTDIR/FASTQC/fastqc.time.log 2>&1
+
+###1. Joining reads with FLASH
+echo "[$(date)]: FLASH starts" >&2
+#NOTE: flash needs output directory (-d) and outputprefix (-o) seperately!!!
+/usr/bin/time -v flash2 -M 250 -t 10 \
+  -d $OUTDIR/FLASH_OUTPUT/ \
+  -o ${SAMPLEN} \
+  ${FR1} ${FR1/_R1/_R2} \
+  > $OUTDIR/FLASH_OUTPUT/${SAMPLEN}.time.log 2>&1
+echo "[$(date)]: FLASH done" >&2
+
+###2.Run YARA on both joined and unjoined reads 
+echo "[$(date)]: YARA starts" >&2
+/usr/bin/time -v yara_mapper -v -t 10 -s 2 \
+  ${YARA_DB} \
+  $OUTDIR/FLASH_OUTPUT/${SAMPLEN}.extendedFrags.fastq \
+  -o $OUTDIR/${SAMPLEN}.join.bam \
+  > $OUTDIR/${SAMPLEN}.join.time.log 2>&1
+/usr/bin/time -v yara_mapper -v -t 10 -s 2 \
+  ${YARA_DB} \
+  $OUTDIR/FLASH_OUTPUT/${SAMPLEN}.notCombined_1.fastq \
+  $OUTDIR/FLASH_OUTPUT/${SAMPLEN}.notCombined_2.fastq \
+  -o $OUTDIR/${SAMPLEN}.un.bam \
+  > $OUTDIR/${SAMPLEN}.un.time.log 2>&1
+echo "[$(date)]: YARA finished" >&2
+
+
+####removing the FASTQJOIN file after mapping it with yara
+#rm $OUTDIR/FLASH_OUTPUT/${SAMPLEN}.extendedFrags.fastq
+#rm $OUTDIR/FLASH_OUTPUT/${SAMPLEN}.notCombined_1.fastq
+#rm $OUTDIR/FLASH_OUTPUT/${SAMPLEN}.notCombined_2.fastq
+
+
+###3. Merge the bam files with samtools 
+echo "[$(date)]: Samtools merge starts" >&2
+/usr/bin/time -v samtools merge --threads 10 \
+  $OUTDIR/${SAMPLEN}.merged.bam $OUTDIR/${SAMPLEN}.un.bam \
+  $OUTDIR/${SAMPLEN}.join.bam \
+  >$OUTDIR/${SAMPLEN}.merged.time.log 2>&1
+echo "[$(date)]: Samtools merge done" >&2
+
+####removing the intermediate bam files 
+rm -f $OUTDIR/${SAMPLEN}.join.bam $OUTDIR/${SAMPLEN}.un.bam
+
+###4. Run SLIMM 
+echo "[$(date)]: SLIMM starts" >&2
+#NOTE: Wrong bam file used here!!!
+/usr/bin/time -v slimm -w 1000 -r species \
+  -ro -co -o $OUTDIR/slimm_reports/${SAMPLEN} \
+  ${SLIMM_DB} \
+  $OUTDIR/${SAMPLEN}.merged.bam \
+  > $OUTDIR/slimm_reports/${SAMPLEN}.time.log 2>&1
+echo "[$(date)]: SLIMM done" >&2
+
